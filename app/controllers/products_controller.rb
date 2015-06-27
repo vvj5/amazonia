d70202bd2eb081b29647aa9b849d59e5dfceb5cc
Will Sherman <vvj5@icloud.com>
2015-06-27 14:19:04 -0500

    Styling Products Table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,13 @@ class ProductsController < ApplicationController
 
   def index
     @product = Product.all
+    # render html: Product.all
   end
+
+  def show
+    render html: Product.find(params[:id])
+  end
+
 
 
 end
